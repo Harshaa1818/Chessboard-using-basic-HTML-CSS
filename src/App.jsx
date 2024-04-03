987fec3806652694ecb26d8879232e320c8b0dec
Harshaa1818 <114155862+Harshaa1818@users.noreply.github.com>
2024-04-03 14:59:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import React from "react"
+
+
+
+function App() {
+ 
+  
+  
+
+  return (
+    <>
+      
+  <container>
+    <div className="row">
+      <box id="even">1</box>
+      <box id="odd">2</box>
+      <box id="even">3</box>
+      <box id="odd">4</box>
+      <box id="even">5</box>
+      <box id="odd">6</box>
+      <box id="even">7</box>
+      <box id="odd">8</box>
+    </div>
+    <div className="row">
+    <box id="odd">1</box>
+    <box id="even">2</box>
+    <box id="odd">3</box>
+    <box id="even"></box>
+    <box id="odd"></box>
+    <box id="even"></box>
+    <box id="odd"></box>
+    <box id="even"></box>
+    </div>
+    <div className="row">
+      <box id="even"></box>
+      <box id="odd"></box>
+      <box id="even"></box>
+      <box id="odd"></box>
+      <box id="even"></box>
+      <box id="odd"></box>
+      <box id="even"></box>
+      <box id="odd"></box>
+    </div>
+    <div className="row">
+    <box id="odd"></box>
+    <box id="even"></box>
+    <box id="odd"></box>
+    <box id="even"></box>
+    <box id="odd"></box>
+    <box id="even"></box>
+    <box id="odd"></box>
+    <box id="even"></box>
+    </div>
+    <div className="row">
+      <box id="even"></box>
+      <box id="odd"></box>
+      <box id="even"></box>
+      <box id="odd"></box>
+      <box id="even"></box>
+      <box id="odd"></box>
+      <box id="even"></box>
+      <box id="odd"></box>
+    </div>
+    <div className="row">
+    <box id="odd"></box>
+    <box id="even"></box>
+    <box id="odd"></box>
+    <box id="even"></box>
+    <box id="odd"></box>
+    <box id="even"></box>
+    <box id="odd"></box>
+    <box id="even"></box>
+    </div>
+    <div className="row">
+      <box id="even">1</box>
+      <box id="odd">2</box>
+      <box id="even">3</box>
+      <box id="odd">4</box>
+      <box id="even">5</box>
+      <box id="odd">6</box>
+      <box id="even">7</box>
+      <box id="odd">8</box>
+    </div>
+    <div className="row">
+    <box id="odd">1</box>
+    <box id="even">2</box>
+    <box id="odd">3</box>
+    <box id="even">4</box>
+    <box id="odd">5</box>
+    <box id="even">6</box>
+    <box id="odd">7</box>
+    <box id="even">8</box>
+    </div>
+  </container>
+    
+     
+      
+  
+      
+    </>
+  )
+}
+
+export default App
